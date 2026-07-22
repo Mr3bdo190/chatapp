4df7b3392a63246b1364a5f96b37ed80968df22c
Mr3bdo190 <abdosayed0120@Gmail.com>
2026-07-22 08:34:01 +0300

    Fix: Correct firebase_auth imports in all remaining views

--- lib/features/admin/views/admin_dashboard_view.dart
+++ lib/features/admin/views/admin_dashboard_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/package:firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../../../core/constants/app_constants.dart';
 import '../../../core/routes/app_routes.dart';
 
